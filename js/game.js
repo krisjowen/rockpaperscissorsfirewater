dce9944c3152f750482881371431483f1191e103
krisjowen <24865229+krisjowen@users.noreply.github.com>
2025-02-19 21:10:09 +0000

    Add playerMove rock to outcome function

--- js/game.js
+++ js/game.js
@@ -10,14 +10,24 @@ function newGame() {
     showScore();
 };
 
+function outcome(playerMove, computerMove) {
+    if (playerMove = "rock") {
+        if (computerMove == "scissors" || computerMove == "water") {
+            outcome = "playerWin";
+            playerScore++;
+        } else if (computerMove == "paper" || computerMove == "fire") {
+            outcome = "computerWin";
+            computerScore++;
+        } else {
+            outcome = "tie";
+        };
+    };
+};
+
 function showScore() {
     document.getElementById("playerScore").innerText = game.playerScore;
     document.getElementById("computerScore").innerText = game.computerScore;
 };
-
-function outcome(playerMove, computerMove) {
-
-}
 
 if (typeof module === 'object') {
 module.exports = { game, newGame };
