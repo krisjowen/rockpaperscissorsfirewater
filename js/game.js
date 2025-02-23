c0edc2427862f08747f97fe5949ffedd5206e9db
krisjowen <24865229+krisjowen@users.noreply.github.com>
2025-02-23 22:56:22 +0000

    Modify functions to pass moves to outcome function

--- js/game.js
+++ js/game.js
@@ -1,59 +1,113 @@
 let game = {
-    playerScore: 0,
-    computerScore: 0,
-    turnInProgress: false,
-    choices: ["rock", "paper", "scissors", "fire", "water"]
+  playerScore: 0,
+  computerScore: 0,
+  turnInProgress: false,
+  choices: ["rock", "paper", "scissors", "fire", "water"],
 };
 
-document.getElementById("playerScore").innerText = game.playerScore;
-document.getElementById("computerScore").innerText = game.computerScore;
+// Function to update displayed scores
+function updateScores() {
+  document.getElementById("playerScore").innerText = game.playerScore;
+  document.getElementById("computerScore").innerText = game.computerScore;
+}
 
-const divs = document.querySelectorAll('.playerInput');
-
-
+// Initialize score display
+updateScores();
+const divs = document.querySelectorAll(".playerInput");
 
 divs.forEach((div) => {
-    div.addEventListener('click', (event) => {
-         console.log("Player Move:", event.srcElement.id);
-         computerMove();
-         let playerMove = event.srcElement.id;
-         return playerMove;
-    });
+  div.addEventListener("click", (event) => {
+    console.log("Player Move:", event.srcElement.id);
+    let playerMove = event.srcElement.id;
+    let computerMoveResult = computerMove();
+    outcome(playerMove, computerMoveResult);
+  });
 });
 
 function computerMove() {
-    console.log("Computer Move:", game.choices[(Math.floor(Math.random() * 5))]);
-    outcome();
-    return game.choices[(Math.floor(Math.random() * 5))];
-};
+  let move = game.choices[Math.floor(Math.random() * 5)];
+  console.log("Computer Move:", move);
+  return move;
+}
 
-function outcome() {
-    let a = playerMove;
-    let b = computerMove();
+function outcome(playerMove, computerMove) {
+  let result = "";
 
-    if (a == "rock") {
-        if (b == "scissors" || b == "water") {
-            result = "playerWin";
-            game.playerScore++;
-        } else if (computerMove == "paper" || computerMove == "fire") {
-            result = "computerWin";
-            game.computerScore++;
+  if (playerMove == computerMove) {
+    result = "tie";
+
+    // playerMove is rock
+  } else if (playerMove == "rock") {
+    if (computerMove == "scissors" || computerMove == "water") {
+      result = "playerWin";
+      game.playerScore++;
+    } else {
+      result = "computerWin";
+      game.computerScore++;
+    }
+}
+      // playerMove is paper
+    else if (playerMove == "paper") {
+      if (computerMove == "rock" || computerMove == "water") {
+        result = "playerWin";
+        game.playerScore++;
+      } else {
+        result = "computerWin";
+        game.computerScore++;
+      }
+    }
+
+        //playerMove is scissors
+       else if (playerMove == "scissors") {
+        if (computerMove == "paper" || computerMove == "water") {
+          result = "playerWin";
+          game.playerScore++;
         } else {
-            result = "tie";
-        };
-    } else if (playerMove = "paper") {
-        if (computerMove == "rock" || computerMove == "water") {
-            result = "playerWin";
-            playerScore++
+          result = "computerWin";
+          game.computerScore++;
         }
     }
-};
+
+          //playerMove is fire
+         else if (playerMove == "fire") {
+            if (computerMove == "water") {
+                result = "computerWin";
+                game.computerScore++;
+            } else {
+                result = "playerWin";
+                game.playerScore++;
+            }
+            } 
+            
+            //playerMove is water
+            else if (playerMove == "water") {
+                if (computerMove == "fire") {
+                    result = "playerWin";
+                    game.playerScore++;
+                } else {
+                    result = "computerWin";
+                    game.computerScore++;
+                }
+            }
+
+      // Display result
+      console.log(result);
+
+      // Update score display
+      updateScores();
+        }
+
+
+
+
+
+
 
 function newGame() {
-    game.playerScore = 0;
-    game.computerScore = 0;
-};
+  game.playerScore = 0;
+  game.computerScore = 0;
+}
 
-if (typeof module === 'object') {
-module.exports = { game, newGame };
-};
+if (typeof module === "object") {
+  module.exports = { game, newGame };
+}
